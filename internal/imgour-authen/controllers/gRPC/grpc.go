b9a15e456590ba192306301151f22fc94d78bc51
huynx3 <huynx3@vng.com.vn>
2022-06-19 16:13:40 +0700

    pre implement auth grpc

--- internal/imgour-authen/controllers/gRPC/grpc.go
+++ internal/imgour-authen/controllers/gRPC/grpc.go
@@ -7,11 +7,21 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const CERTIFICATE = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhki...\n-----END PUBLIC KEY-----"
+
 type gRpcServer struct {
 	pb.UnimplementedAuthServiceServer
 }
 
-func (gRpcServer) Validate(ctx context.Context, req *pb.ValidateRequest) (*pb.ValidateResponse, error) {
+func (gRpcServer) GetRoles(ctx context.Context, req *pb.GetRolesRequest) (*pb.GetRolesResponse, error) {
+	out := new(pb.GetRolesResponse)
+
+	uid := req.GetUid()
+	if uid == "" {
+		return nil, status.Errorf(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	return out, nil
 	return nil, status.Errorf(codes.Unimplemented, "method Validate not implemented")
 }
 func (gRpcServer) GetUserProfile(context.Context, *pb.GetUserProfileRequest) (*pb.GetUserProfileResponse, error) {
